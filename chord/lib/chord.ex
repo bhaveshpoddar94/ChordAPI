b4117d56b198da9e50e5ad64ce1f0b8a997f0cb7
Bhavesh Poddar <poddar.bhavesh94@gmail.com>
2018-10-19 16:30:04 -0400

    #3. Implement a generic hash method which takes a list of data and returns a list of tuples where first element is the id and second element is the data itself.

--- chord/lib/chord.ex
+++ chord/lib/chord.ex
@@ -5,6 +5,9 @@ defmodule Chord do
 
     # choose m for chord ring
     m = choose_m(8, numNodes)
+
+    # hash pids
+    pid_list = hash(nodes, m)
   end
 
   # loop for choosing m
@@ -13,6 +16,13 @@ defmodule Chord do
       numNodes < :math.pow(2, m) -> m
       true -> choose_m(m+step, numNodes)
     end
+  end
+
+  defp hash(data, m) do
+    Enum.map(data, fn datum -> {inspect(datum), datum} end)
+    |> Enum.map(fn {value, datum} -> {:crypto.hash(:sha, value), datum} end)
+    |> Enum.map(fn {hash,  datum} -> {binary_part(hash, 0, trunc(m/8)), datum} end)
+    |> Enum.map(fn {sliced_hash, datum} -> {:binary.decode_unsigned(sliced_hash), datum} end)
   end
 end
 
