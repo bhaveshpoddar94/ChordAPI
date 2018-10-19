3281e7f05a8a61e3864cf9509b14e6f4dffb50ae
Bhavesh Poddar <poddar.bhavesh94@gmail.com>
2018-10-19 16:26:30 -0400

    #1. Spawn nodes of type Genserver. These represent the nodes on the Chord ring and will be responsible for implementing the lookup algorithm.

--- chord/lib/chord.ex
+++ chord/lib/chord.ex
@@ -1,18 +1,25 @@
 defmodule Chord do
-  @moduledoc """
-  Documentation for Chord.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Chord.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def init(numNodes, numRequests) do
+    # spawn nodes
+    nodes = Enum.map(1..numNodes, fn node -> Node.start_link(node) end)
   end
 end
+
+defmodule Node do
+  use GenServer
+
+  # Client
+  def start_link(state) do
+    {:ok, pid} = GenServer.start_link(__MODULE__, state)
+    pid
+  end
+
+  # Server
+  def init(state) do
+    {:ok, state}
+  end
+end
+
+[numNodes, numRequests] = System.argv() 
+                          |> Enum.map(fn arg -> String.to_integer(arg) end)
+Chord.init(numNodes, numRequests)
