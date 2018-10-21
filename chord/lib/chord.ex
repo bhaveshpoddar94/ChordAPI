1a34a36985eb5d49db171fd89f060169b4b6e820
Bhavesh Poddar <poddar.bhavesh94@gmail.com>
2018-10-21 03:25:42 -0400

    #5. Add cast method to set state of the nodes. The async method is used to add parent node and finger table to the state of a node.

--- chord/lib/chord.ex
+++ chord/lib/chord.ex
@@ -1,18 +1,30 @@
 defmodule Chord do
   def init(numNodes, numRequests) do
     # spawn nodes
-    nodes = Enum.map(1..numNodes, fn node -> Node.start_link(node) end)
+    nodes = Enum.map(1..numNodes, fn node -> Node.start_link(%{}) end)
 
     # choose m for chord ring
     m = choose_m(8, numNodes)
 
     # hash pids
-    pid_list = hash(nodes, m)
+    pid_list = hash(nodes, m) |> Enum.sort
 
     # hash keys
-    keys  = generate_keys(numNodes * 5)
-    IO.inspect keys
-    key_list = hash(keys, m)
+    keys  = generate_keys(numNodes * @multiplicity)
+    key_list = hash(keys, m) |> Enum.sort
+
+    # create fingertables
+    finger_tables = create_fingertables(pid_list, m)
+    Enum.map(finger_tables, fn {pid, table} -> Node.set_table(pid, table) end)
+
+    def loop(0), do: :ok
+    def loop(N) do
+      receive do
+        {:CHECK, pid, value} ->
+          IO.inspect numNodes
+          loop(numNodes-1)
+      end
+    end
   end
 
   # loop for choosing m
@@ -24,13 +36,7 @@ defmodule Chord do
   end
 
   defp generate_keys(length) do
-    Enum.map(1..length, fn _x -> gen_util(8))
-  end
-
-  @bytes "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-  defp gen_util(n) do
-    Enum.map(1..n, fn _x -> :binary.at(@bytes, :rand.uniform(byte_size(@bytes) - 1)) end) 
-    |> List.to_string
+    Enum.map(1..length, fn _x -> gen_util(8) end)
   end
 
   defp hash(data, m) do
@@ -38,6 +44,28 @@ defmodule Chord do
     |> Enum.map(fn {value, datum} -> {:crypto.hash(:sha, value), datum} end)
     |> Enum.map(fn {hash,  datum} -> {binary_part(hash, 0, trunc(m/8)), datum} end)
     |> Enum.map(fn {sliced_hash, datum} -> {:binary.decode_unsigned(sliced_hash), datum} end)
+  end
+
+  defp create_fingertables(nodes, m) do
+    ring_size = :math.pow(2, m) |> trunc
+    Enum.map(nodes, fn {id, pid} -> {pid, loop_m(id, nodes, m, ring_size)} end)
+  end
+
+  defp loop_m(curr_id, nodes, m, ring_size) do
+    Enum.map(1..m, fn k ->
+      (curr_id + :math.pow(2, k-1)) |> trunc |> Integer.mod(ring_size)
+      |> loop_nodes(nodes, 0) end)
+  end
+
+  defp loop_nodes(lim, nodes, index) when index >= length(nodes) do
+    Enum.at(nodes, 0)
+  end
+  defp loop_nodes(lim, nodes, index) do
+    {id, pid} = Enum.at(nodes, index)
+    cond do
+      id >= lim -> {id, pid}
+      true -> loop_nodes(lim, nodes, index+1)
+    end
   end
 end
 
@@ -50,12 +78,29 @@ defmodule Node do
     pid
   end
 
+  def set_state(pid, key, value) do
+    GenServer.cast(pid, {:set_state, key, value})
+  end
+
+  def get_table(pid) do
+    GenServer.call(pid, :get_table)
+  end
+
   # Server
   def init(state) do
     {:ok, state}
   end
+
+  def handle_cast({:set_state, key, value}, state) do
+    state = Map.put(state, key, value)
+    {:noreply, state}
+  end
+
+  def handle_call(:get_table, _from, state) do
+    {:reply, state[:table], state}
+  end
+
 end
 
-[numNodes, numRequests] = System.argv() 
-                          |> Enum.map(fn arg -> String.to_integer(arg) end)
+[numNodes, numRequests] = System.argv() |> Enum.map(fn arg -> String.to_integer(arg) end)
 Chord.init(numNodes, numRequests)
